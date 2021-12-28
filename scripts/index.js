81de7d18fdedb1edd9647ebdd256037c1c97cb02
hiepnguyendt <89370886+hiepnguyendt@users.noreply.github.com>
2021-12-28 19:43:52 +0700

    update

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 02, 2021',
+  birthdate: 'Jan 02, 2022',
   name: 'Hanh'
 };
 
